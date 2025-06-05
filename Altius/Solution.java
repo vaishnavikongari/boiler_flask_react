059faaa830afc0fd31e8da6b48c70d432efabae8
vaishnavikongari <vaishnavi8kongari@gmail.com>
2025-06-05 12:40:43 +0530

    Add files via upload

---
+++ Altius/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.*;
+// import java.utils.arrays.binarySearch;
+
+public class Solution {
+    public static int longestSequence(int[] nums) {
+        int dp[] = new int[nums.length];
+        int n = nums.length;
+        for (int j = 0; j < n; j++) {
+            dp[i] = java.util.arrays.binarySearch(dp, 0, len, nums);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len)
+                len++;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int c = longestSequence(arr);
+        System.out.print(c);
+    }
+}
